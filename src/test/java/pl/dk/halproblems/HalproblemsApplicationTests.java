2052e705e628a4f15731974d057096e3aab7d0b9
dkublik <dawid.kublik@vimn.com>
2021-07-14 17:40:33 +0200

    initial

---
+++ src/test/java/pl/dk/halproblems/HalproblemsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dk.halproblems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalproblemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
