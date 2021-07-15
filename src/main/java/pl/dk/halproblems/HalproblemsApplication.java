21457603a4bbc30b60d68db237d3bb41238d7ec2
dkublik <dawid.kublik@vimn.com>
2021-07-15 10:22:13 +0200

    webmvc removed

--- src/main/java/pl/dk/halproblems/HalproblemsApplication.java
+++ src/main/java/pl/dk/halproblems/HalproblemsApplication.java
@@ -2,8 +2,13 @@ package pl.dk.halproblems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
+import org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType;
+
+import static org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType.HAL;
 
 @SpringBootApplication
+@EnableHypermediaSupport(type = HAL)
 public class HalproblemsApplication {
 
 	public static void main(String[] args) {
